618ae4bd67c4241268962bedc2d95040da7c85ca
Famanias <haratayo@gmail.com>
2024-12-12 02:29:39 +0800

    update main.dart

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -130,7 +129,7 @@ class MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'TableServe Admin',
+      title: 'TableServe',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         fontFamily: 'Poppins',
